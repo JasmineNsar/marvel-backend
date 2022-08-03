e4b73f9070f5097e5b7744c0ea1b8498e7fa7d80
JasmineNsar <jasminensar@outlook.com>
2022-08-03 19:24:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+// const mongoose = require("mongoose");
+const axios = require("axios");
+
+const app = express();
+app.use(cors());
+
+// mongoose.connect(process.env.MONGODB_URI);
+
+// ⬇︎ ROUTES AVEC REQUETES API VIA AXIOS
+
+app.get("/", async (req, res) => {
+  res.json({ message: "welcome" });
+});
+
+app.get("/characters", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}`
+    );
+    console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.get("/comics", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.all("*", (req, res) => {
+  console.log("route not found");
+  res.status(404).json({ message: "route not found" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server started");
+});
